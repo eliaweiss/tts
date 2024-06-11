f2922598ddbd41cccc1e507d32ce5627120d621a
Elia <elia.weiss@gmail.com>
2024-06-11 17:56:43 +0300

    1

--- phrases.js
+++ phrases.js
@@ -1,7 +1,12 @@
-// Example usage
 const phrases = [
-    { pt: 'Olá, como você está?', en: 'Hi, how are you?' },
-    { pt: 'Bom dia, prazer em conhecê-lo.', en: 'Good morning, nice to meet you.' },
-    { pt: 'Obrigado por sua ajuda.', en: 'Thank you for your help.' },
-    { pt: 'Até logo!', en: 'See you later!' }
-];
+    { pt: 'Qual é o seu nome?', en: 'What is your name?' },
+    { pt: 'Qual é a sua idade?', en: 'How old are you?' },
+    { pt: 'Qual é a sua profissão?', en: 'What is your profession?' },
+    { pt: 'Qual é o seu endereço?', en: 'What is your address?' },
+    { pt: 'Qual é o seu número de telefone?', en: 'What is your phone number?' },
+    { pt: 'Qual é a sua nacionalidade?', en: 'What is your nationality?' },
+    { pt: 'Qual é o seu time de futebol favorito?', en: 'What is your favorite soccer team?' },
+    { pt: 'Qual é a data de hoje?', en: 'What is today\'s date?' },
+    { pt: 'Qual é o seu filme favorito?', en: 'What is your favorite movie?' },
+    { pt: 'Qual é a sua comida favorita?', en: 'What is your favorite food?' }
+    ];
