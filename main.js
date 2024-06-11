bb9b98c2aa9800c35b905cff8507ad095a588f55
Elia <elia.weiss@gmail.com>
2024-06-11 17:53:32 +0300

    1

--- main.js
+++ main.js
@@ -47,17 +47,6 @@ function createPhrases(phrases) {
 }
 
 
-
-// Example usage
-const phrases = [
-    { pt: 'Olá, como você está?', en: 'Hi, how are you?' },
-    { pt: 'Bom dia, prazer em conhecê-lo.', en: 'Good morning, nice to meet you.' },
-    { pt: 'Obrigado por sua ajuda.', en: 'Thank you for your help.' },
-    { pt: 'Até logo!', en: 'See you later!' }
-];
-
-
-
 let voices = [];
 
 ///////////////////////////////////
